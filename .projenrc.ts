fdb609e85cbac1e8b50a5cf6a6099db35b0685d9
Niko Virtala <niko.virtala@hey.com>
2023-12-27 14:47:59 +0200

    feat: aws cdk typescript app with tsx and esm

---
+++ .projenrc.ts
@@ -0,0 +1,24 @@
+import { cdk, javascript } from "projen";
+const project = new cdk.JsiiProject({
+  author: "Niko Virtala",
+  authorAddress: "niko.virtala@hey.com",
+  defaultReleaseBranch: "main",
+  deps: ["projen"],
+  description:
+    "A projen template for AWS CDK TypeScript app. with ES Modules and TSX.",
+  jest: false,
+  jsiiVersion: "~5.0.0",
+  license: "MIT",
+  licensed: true,
+  minNodeVersion: "18.18.0",
+  name: "projen-aws-cdk-app",
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  packageManager: javascript.NodePackageManager.YARN_CLASSIC,
+  packageName: "@nikovirtala/projen-aws-cdk-app",
+  peerDeps: ["projen", "constructs"],
+  prettier: true,
+  projenrcTs: true,
+  releaseToNpm: true,
+  repositoryUrl: "https://github.com/nikovirtala/projen-aws-cdk-app.git",
+});
+project.synth();
