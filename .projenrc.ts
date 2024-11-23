bb084cf1676be8753920d396ae012dce4a2a1a65
Niko Virtala <niko.virtala@hey.com>
2024-11-23 15:13:23 +0200

    chore: adjust prettier settings

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,46 +1,57 @@
 import { cdk, javascript } from "projen";
 
 const project = new cdk.JsiiProject({
-  author: "Niko Virtala",
-  authorAddress: "niko.virtala@hey.com",
-  defaultReleaseBranch: "main",
-  deps: ["projen"],
-  description: "A projen template for AWS CDK TypeScript app. with ES Modules and TSX.",
-  dependabot: false,
-  depsUpgradeOptions: {
-    workflowOptions: {
-      labels: ["auto-approve", "auto-merge"],
+    author: "Niko Virtala",
+    authorAddress: "niko.virtala@hey.com",
+    defaultReleaseBranch: "main",
+    deps: ["projen"],
+    description: "A projen template for AWS CDK TypeScript app. with ES Modules and TSX.",
+    dependabot: false,
+    depsUpgradeOptions: {
+        workflowOptions: {
+            labels: ["auto-approve", "auto-merge"],
+        },
     },
-  },
-  autoApproveOptions: {
-    secret: "GITHUB_TOKEN",
-    allowedUsernames: ["nikovirtala"],
-  },
-  mergify: true,
-  autoMerge: true,
-  jest: false,
-  jsiiVersion: "~5.5.0",
-  license: "MIT",
-  licensed: true,
-  minNodeVersion: "22.11.0",
-  name: "projen-aws-cdk-app",
-  npmAccess: javascript.NpmAccess.PUBLIC,
-  packageManager: javascript.NodePackageManager.PNPM,
-  packageName: "@nikovirtala/projen-aws-cdk-app",
-  peerDeps: ["projen", "constructs"],
-  pnpmVersion: "9",
-  prettier: true,
-  prettierOptions: {
-    settings: {
-      printWidth: 120,
-      tabWidth: 2,
-      trailingComma: javascript.TrailingComma.ALL,
+    autoApproveOptions: {
+        secret: "GITHUB_TOKEN",
+        allowedUsernames: ["nikovirtala"],
     },
-  },
-  projenrcTs: true,
-  releaseToNpm: true,
-  repositoryUrl: "https://github.com/nikovirtala/projen-aws-cdk-app.git",
-  typescriptVersion: "5.7.2",
+    mergify: true,
+    autoMerge: true,
+    jest: false,
+    jsiiVersion: "~5.5.0",
+    license: "MIT",
+    licensed: true,
+    minNodeVersion: "22.11.0",
+    name: "projen-aws-cdk-app",
+    npmAccess: javascript.NpmAccess.PUBLIC,
+    packageManager: javascript.NodePackageManager.PNPM,
+    packageName: "@nikovirtala/projen-aws-cdk-app",
+    peerDeps: ["projen", "constructs"],
+    pnpmVersion: "9",
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 120,
+            tabWidth: 4,
+            trailingComma: javascript.TrailingComma.ALL,
+        },
+    },
+    projenrcTs: true,
+    releaseToNpm: true,
+    repositoryUrl: "https://github.com/nikovirtala/projen-aws-cdk-app.git",
+    typescriptVersion: "5.7.2",
+});
+
+project.vscode?.extensions.addRecommendations("dbaeumer.vscode-eslint", "esbenp.prettier-vscode");
+
+project.vscode?.settings.addSettings({
+    "editor.codeActionsOnSave": {
+        "source.fixAll": "explicit",
+    },
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.formatOnSave": true,
+    "editor.tabSize": 4,
 });
 
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -2,108 +2,108 @@ import * as path from "path";
 import { awscdk, javascript } from "projen";
 
 export class AwsCdkApp extends awscdk.AwsCdkTypeScriptApp {
-  constructor(options: awscdk.AwsCdkTypeScriptAppOptions) {
-    const {
-      cdkVersion,
-      cdkVersionPinning,
-      defaultReleaseBranch,
-      jest,
-      minNodeVersion,
-      prettier,
-      prettierOptions,
-      sampleCode,
-      tsconfig,
-      typescriptVersion,
-      ...awsCdkTypeScriptAppOptions
-    } = options;
+    constructor(options: awscdk.AwsCdkTypeScriptAppOptions) {
+        const {
+            cdkVersion,
+            cdkVersionPinning,
+            defaultReleaseBranch,
+            jest,
+            minNodeVersion,
+            prettier,
+            prettierOptions,
+            sampleCode,
+            tsconfig,
+            typescriptVersion,
+            ...awsCdkTypeScriptAppOptions
+        } = options;
 
-    const defaultTsConfig = {
-      compilerOptions: {
-        allowImportingTsExtensions: true,
-        allowSyntheticDefaultImports: true,
-        alwaysStrict: true,
-        declaration: true,
-        esModuleInterop: true,
-        experimentalDecorators: true,
-        inlineSourceMap: true,
-        inlineSources: true,
-        isolatedModules: true,
-        lib: ["esnext"],
-        module: "nodenext",
-        moduleResolution: javascript.TypeScriptModuleResolution.NODE_NEXT,
-        noEmitOnError: false,
-        noFallthroughCasesInSwitch: true,
-        noImplicitAny: true,
-        noImplicitOverride: true,
-        noImplicitReturns: true,
-        noImplicitThis: true,
-        noUnusedLocals: true,
-        noUnusedParameters: true,
-        resolveJsonModule: true,
-        strict: true,
-        strictNullChecks: true,
-        strictPropertyInitialization: true,
-        stripInternal: true,
-        target: "esnext",
-      },
-    };
+        const defaultTsConfig = {
+            compilerOptions: {
+                allowImportingTsExtensions: true,
+                allowSyntheticDefaultImports: true,
+                alwaysStrict: true,
+                declaration: true,
+                esModuleInterop: true,
+                experimentalDecorators: true,
+                inlineSourceMap: true,
+                inlineSources: true,
+                isolatedModules: true,
+                lib: ["esnext"],
+                module: "nodenext",
+                moduleResolution: javascript.TypeScriptModuleResolution.NODE_NEXT,
+                noEmitOnError: false,
+                noFallthroughCasesInSwitch: true,
+                noImplicitAny: true,
+                noImplicitOverride: true,
+                noImplicitReturns: true,
+                noImplicitThis: true,
+                noUnusedLocals: true,
+                noUnusedParameters: true,
+                resolveJsonModule: true,
+                strict: true,
+                strictNullChecks: true,
+                strictPropertyInitialization: true,
+                stripInternal: true,
+                target: "esnext",
+            },
+        };
 
-    super({
-      cdkVersion: !cdkVersion || cdkVersion === "2.1.0" ? "2.170.0" : cdkVersion, // this does not work!
-      cdkVersionPinning: cdkVersionPinning ?? true,
-      defaultReleaseBranch: defaultReleaseBranch ?? "main",
-      jest: jest ?? false,
-      minNodeVersion: minNodeVersion ?? "22.11.0",
-      prettier: prettier ?? true,
-      prettierOptions: prettierOptions ?? {
-        settings: {
-          printWidth: 120,
-          tabWidth: 4,
-          trailingComma: javascript.TrailingComma.ALL,
-        },
-      },
-      projenrcTs: true,
-      sampleCode: sampleCode ?? true,
-      tsconfig: {
-        compilerOptions: {
-          ...defaultTsConfig.compilerOptions,
-          ...(tsconfig?.compilerOptions ?? {}),
-        },
-      },
-      typescriptVersion: typescriptVersion ?? "5.7.2",
-      ...awsCdkTypeScriptAppOptions,
-    });
+        super({
+            cdkVersion: !cdkVersion || cdkVersion === "2.1.0" ? "2.170.0" : cdkVersion, // this does not work!
+            cdkVersionPinning: cdkVersionPinning ?? true,
+            defaultReleaseBranch: defaultReleaseBranch ?? "main",
+            jest: jest ?? false,
+            minNodeVersion: minNodeVersion ?? "22.11.0",
+            prettier: prettier ?? true,
+            prettierOptions: prettierOptions ?? {
+                settings: {
+                    printWidth: 120,
+                    tabWidth: 4,
+                    trailingComma: javascript.TrailingComma.ALL,
+                },
+            },
+            projenrcTs: true,
+            sampleCode: sampleCode ?? true,
+            tsconfig: {
+                compilerOptions: {
+                    ...defaultTsConfig.compilerOptions,
+                    ...(tsconfig?.compilerOptions ?? {}),
+                },
+            },
+            typescriptVersion: typescriptVersion ?? "5.7.2",
+            ...awsCdkTypeScriptAppOptions,
+        });
 
-    // switch commonjs to es modules
-    this.package.addField("type", "module");
+        // switch commonjs to es modules
+        this.package.addField("type", "module");
 
-    // execute code with tsx instead of ts-node
-    this.deps.removeDependency("ts-node");
-    this.addDevDeps("tsx");
-    this.defaultTask?.reset();
-    this.defaultTask?.exec(`tsx --tsconfig ${this.tsconfigDev?.file.path} .projenrc.ts`);
+        // execute code with tsx instead of ts-node
+        this.deps.removeDependency("ts-node");
+        this.addDevDeps("tsx");
+        this.defaultTask?.reset();
+        this.defaultTask?.exec(`tsx --tsconfig ${this.tsconfigDev?.file.path} .projenrc.ts`);
 
-    this.cdkConfig.json.addOverride(
-      "app",
-      `tsx --tsconfig ${this.tsconfig?.file.path} ${path.posix.join(this.srcdir, this.appEntrypoint)}`,
-    );
+        this.cdkConfig.json.addOverride(
+            "app",
+            `tsx --tsconfig ${this.tsconfig?.file.path} ${path.posix.join(this.srcdir, this.appEntrypoint)}`,
+        );
 
-    // lint code with modern ecma version
-    this.tryFindObjectFile(".eslintrc.json")?.addOverride("parserOptions.ecmaVersion", "latest");
+        // lint code with modern ecma version
+        this.tryFindObjectFile(".eslintrc.json")?.addOverride("parserOptions.ecmaVersion", "latest");
 
-    this.eslint?.addRules({
-      "@typescript-eslint/await-thenable": "error",
-    });
+        this.eslint?.addRules({
+            "@typescript-eslint/await-thenable": "error",
+        });
 
-    this.vscode?.extensions.addRecommendations("dbaeumer.vscode-eslint", "esbenp.prettier-vscode");
+        this.vscode?.extensions.addRecommendations("dbaeumer.vscode-eslint", "esbenp.prettier-vscode");
 
-    this.vscode?.settings.addSettings({
-      "editor.codeActionsOnSave": {
-        "source.fixAll": "explicit",
-      },
-      "editor.defaultFormatter": "esbenp.prettier-vscode",
-      "editor.formatOnSave": true,
-      "editor.tabSize": 4,
-    });
-  }
+        this.vscode?.settings.addSettings({
+            "editor.codeActionsOnSave": {
+                "source.fixAll": "explicit",
+            },
+            "editor.defaultFormatter": "esbenp.prettier-vscode",
+            "editor.formatOnSave": true,
+            "editor.tabSize": 4,
+        });
+    }
 }
